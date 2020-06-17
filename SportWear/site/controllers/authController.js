b3c62a4ba6b7316c2b122230ab40d3012a4ca400
santilago97 <s.lagocurretti@gmail.com>
2020-06-17 17:25:12 -0300

    agrego validation al register

--- SportWear/site/controllers/authController.js
+++ SportWear/site/controllers/authController.js
@@ -1,12 +1,31 @@
-module.exports = {
+const { check, validationResult, body } = require('express-validator');
+const bcrypt = require('bcrypt');
+const userData = require('../models/user');
 
+
+module.exports = {
     register: function (req, res) {
         res.render ('../../site/views/auth/register')
     },
 
     newUser: function (req, res) {
-        let name = req.body.name;
-        res.send('Usuario ' + name + ' registrado con exito');  
+        let errors= validationResult(req);
+
+        if (errors.isEmpty()) {
+            let user = {
+                name: req.body.name,
+                apellido: req.body.apellido,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 5)
+            }
+
+            userData.create(user);
+            res.send('Usuario ' + req.body.name + ' registrado con exito');
+            //res.redirect('/')
+
+        } else {
+            res.render('auth/register', {errors: errors.errors})
+        }
     },
 
     login: function (req, res) {
--- SportWear/site/routes/auth.js
+++ SportWear/site/routes/auth.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult, body } = require('express-validator')
+const fs = require('fs');
+const userData = require('../models/user');
+
 
 const controller = require('../controllers/authController');
 
 router.get('/register', controller.register);
-router.post('/register', controller.newUser);
+router.post('/register',[
+    check('name').isLength({min: 1}).withMessage('El usuario a registrar debe tener un nombre'),
+    check('apellido').isLength({min: 1}).withMessage('El usuario a registrar debe tener un apellido'),
+    check('email').isEmail().withMessage('El email debe ser valido'),
+    check('password').isLength({min:5}).withMessage('La contraseña debe contener 5 caracteres como minimo')   
+] ,controller.newUser);
 router.get('/login', controller.login);
 router.post('/login', controller.loginExistingUser);
 
