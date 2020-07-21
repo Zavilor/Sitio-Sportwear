84cf610b21e8e306821de810b0b09fc38277cded
Juan <juan.rettori@gmail.com>
2020-07-20 21:23:54 -0300

    Agregando carpeta de dbTesting

---
+++ dbTesting/SportWear/site/database/models/Rol.js
@@ -0,0 +1,31 @@
+
+module.exports = (sequelize, DataTypes) => {
+    
+    const Rol = sequelize.define('Rol', {
+        
+        idRol: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        descripction: {
+            type: DataTypes.STRING(50)
+        }
+    },
+
+    {
+        timestamps : false,
+        tableName : 'ROLES'
+    });
+    
+    Rol.associate = function(models) {
+        // associations can be defined here
+        Rol.hasMany(models.User, {
+            as : "Users",
+            foreingKey : "idRol"
+        });
+    };
+    
+    return Rol;
+    
+};
